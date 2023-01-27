818e23575fee4b2cf8680e638a4a6302ec8edbc4
Bernardo Santiago de Souza <108026375+bernardosan@users.noreply.github.com>
2023-01-27 19:46:44 -0300

    Creating scaffold page to sources with dropdownmenu

--- app/src/main/java/com/bernardosan/newsapp/ui/NewsApp.kt
+++ app/src/main/java/com/bernardosan/newsapp/ui/NewsApp.kt
@@ -88,6 +88,6 @@ fun NavGraphBuilder.bottomNavigation(navController: NavController,
         )
     }
     composable(BottomMenuScreen.Sources.route) {
-        Sources()
+        Sources(newsManager = newsManager)
     }
 }
